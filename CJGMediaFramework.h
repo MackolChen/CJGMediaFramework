16e730383f1394550f1cc6e6dd8d1e3e30fab003
chenjg <chenjg@tiens.com>
2022-10-13 13:50:06 +0800

    INIT CJGMediaFramework

---
+++ CJGMediaFramework.h
@@ -0,0 +1,12 @@
+//
+//  CJGMediaFramework.h
+//  Pods
+//
+//  Created by Chen Jinguo on 2022/10/13.
+//
+
+#ifndef CJGMediaFramework_h
+#define CJGMediaFramework_h
+#import "CJGAVComponent.h"
+
+#endif /* CJGMediaFramework_h */
